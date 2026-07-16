a5d178abbdefd6e77a2c7e3ed64b3f9b64ea07e2
zuorn <zuorn@qq.com>
2026-07-16 18:43:04 +0800

    refactor(themes): 调整主题文件目录结构并导出相关模块
    将主题相关文件移动到/src/themes/目录下，同时创建index.ts统一导出主题相关的工具、hooks和类型定义

---
+++ src/themes/index.ts
@@ -0,0 +1,5 @@
+export { isBuiltinTheme } from "./ThemeManager";
+export { useTheme, ThemeProvider } from "./ThemeContext";
+export type { ThemeName } from "./ThemeContext";
+export { getCodeThemeVariables, getDefaultCodeTheme, CODE_THEMES } from "./codeThemes";
+export type { CustomCodeTheme } from "./codeThemes";
